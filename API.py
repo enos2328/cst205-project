d8e0a451e5522554f1889831faa8dca0efb2903b
Magnus Harboe <49603273+magnusharboe@users.noreply.github.com>
2019-05-08 17:54:48 -0700

    Update API.py

--- API.py
+++ API.py
@@ -12,28 +12,32 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-ingredients = ['chicken', 'rice']
-rank = 2
-payload = {
-    'fillIngredients': False,
-    'ingredients': ingredients,
-    'limitLicense': False,
-    'number': 5,
-    'ranking': rank
-}
 
-api_key = "4889bc9e24mshb28791c820806bep1256c2jsn921e1c55af76"
-
-endpoint = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients"
-
-headers = {
-    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    "X-RapidAPI-Key": api_key
-}
+@app.route('/home')
+def main():
+    return render_template('index.html')
 
 
 @app.route('/recipes')
-def main():
+def recipes():
+    ingredients = ['chicken', 'rice']
+    rank = 2
+    payload = {
+        'fillIngredients': False,
+        'ingredients': ingredients,
+        'limitLicense': False,
+        'number': 5,
+        'ranking': rank
+    }
+
+    api_key = "4889bc9e24mshb28791c820806bep1256c2jsn921e1c55af76"
+
+    endpoint = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients"
+
+    headers = {
+        "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "X-RapidAPI-Key": api_key
+    }
     try:
         r = requests.get(endpoint, params=payload, headers=headers)
         data = r.json()
@@ -45,16 +49,23 @@ def main():
 @app.route('/recipes/<int:recipe_id>')
 def recipe(recipe_id):
     endpoint = f'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{recipe_id}/information'
-        payload = {
-            'id': recipe_id
-        }
+    api_key = "4889bc9e24mshb28791c820806bep1256c2jsn921e1c55af76"
+    payload = {
+        'id': recipe_id
+    }
+    headers = {
+        "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "X-RapidAPI-Key": api_key
+    }
+
     try:
         r = requests.get(endpoint, params=payload, headers=headers)
-        data2 = r.json()
+        data = r.json()
     except:
         print('please try again')
-    return render_template('recipes_info.html', data=data2)  # redering the html template and also passing the information it gathered from the API
+    return render_template('recipes_info.html', data=data)  # redering the html template and also passing the information it gathered from the API
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
