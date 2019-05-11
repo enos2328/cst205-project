36f395994aaafbfce5c041f73a0b3ddf247244fc
enos2328 <aenosara@cumb.edu>
2019-05-11 15:35:30 -0700

    Stuff

--- search_page.py
+++ search_page.py
@@ -17,7 +17,7 @@ bootstrap = Bootstrap(app)
 #################################
 @app.route('/')
 def home():
-	return render_template('search_page.html')
+	return render_template('search.html')
 
 if __name__== '__main__':
     app.run(debug=True)
